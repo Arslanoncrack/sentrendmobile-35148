e9af947289bcd95b75d6dd178a68eccc3f722b51
Arslanoncrack <105243335+Arslanoncrack@users.noreply.github.com>
2022-05-09 23:07:40 +0000

    Update options.js

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 export const globalOptions = {
   name: "sentrendmobile_35148",
-  url: "https://sentrendmobile_35148.botics.co",
+  url: "https://sentrendmobile-35148.botics.co/",
   api: "https://sentrendmobile_35148.botics.co/api/v1"
 }
 export const modulesOptions = {
